99577b79a3b1414d74749ae54a507cc97fb7d98b
Tấn Phát <76056976+tphat2002@users.noreply.github.com>
2021-12-01 14:59:45 +0700

    Add files via upload

---
+++ Week2/Bai1/Bai1.cpp
@@ -0,0 +1,12 @@
+#include "Bai1.h"
+
+int main(){
+    Ngay n1;
+    n1.Xuat();
+    Ngay n2(02, 10, 2014);
+    n2.Xuat();
+    Ngay n3(100, 2, 2010);
+    n3.Xuat();
+    Ngay n4(31);
+    n4.Xuat();
+}
